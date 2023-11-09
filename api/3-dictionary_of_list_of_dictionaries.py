23451e905be2e91eaf710e9fcdc68a00f15948d2
angelo54425 <e.gasana@alustudent.com>
2023-11-09 16:32:15 +0000

    3. Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,16 +6,17 @@ import json
 import requests
 import sys
 if __name__ == "__main__":
-    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    def getTodos(id):
+        link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        res = requests.get(link)
+        return json.loads(res.text)
+    link = "https://jsonplaceholder.typicode.com/users/"
     res = requests.get(link)
-    user = json.loads(res.text)
-    num = sys.argv[1]
-    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
-    res = requests.get(link)
-    todos = json.loads(res.text)
-    data = [{"task": i["title"],
-             "completed": i["completed"],
-             "username": user["username"]} for i in todos]
-    json_data = json.dumps({"{}".format(user["id"]): data})
-    with open("{}.json".format(user["id"]), 'w', encoding='utf-8') as f:
-        f.write(json_data) 
+    users = json.loads(res.text)
+    data = {}
+    for i in users:
+        todos = getTodos(i["id"])
+        data[i["id"]] = [{"task": j["title"], "completed": j["completed"],
+                          "username": i["username"]} for j in todos]
+    with open("todo_all_employees.json", 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data))
